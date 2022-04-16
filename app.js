1a5333256c1643e32b0c48f49dfbec7db9b5f35b
Mari <maringuyene@gmail.com>
2022-04-15 18:31:08 -0700

    indicators

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 const favicon = require('serve-favicon');
 const port = 42069;
 const path = require('path');
+const { captureRejections } = require('events');
 
 app.use(express.static('public'));
 app.use(favicon(path.join(__dirname, "public", "favicon.ico")))
@@ -31,35 +32,129 @@ avatar["parameters"].forEach(element => {
     }
 });
 
+var pcat0res, pcat0rej, pcat1res, pcat1rej, pcat2res, pcat2rej;
+
+oscServer.on("/avatar/parameters/cat0", (msg) => {
+    cat0 = msg[1];
+    pcat0res();
+})
+
+oscServer.on("/avatar/parameters/cat1", (msg) => {
+    cat1 = msg[1];
+    pcat1res();
+})
+
+oscServer.on("/avatar/parameters/cat2", (msg) => {
+    cat2 = msg[1];
+    pcat2res();
+})
+
+// const client = new Client('192.168.100.89', 9000);
+// client.send("/avatar/parameters/cat0", true , () => {
+//     client.send("/avatar/parameters/cat0", false , () => {
+//         client.send("/avatar/parameters/cat1", true , () => {
+//             client.send("/avatar/parameters/cat1", false , () => {
+//                 client.send("/avatar/parameters/cat2", true , () => {
+//                     client.send("/avatar/parameters/cat2", false , () => {
+//                         client.close();
+//                     })
+//                 })
+//             })
+//         })
+//     })
+// })
+
+function Cat0() {
+    return {
+    p: new Promise((res,rej) => {
+        pcat0res = res;
+        pcat0rej = rej;
+    }),
+    t : setTimeout(() => pcat0rej("p1"), 250)
+    };
+}
+
+function Cat1() {
+    return {
+    p: new Promise((res,rej) => {
+        pcat1res = res;
+        pcat1rej = rej;
+    }),
+    t : setTimeout(() => pcat1rej("p1"), 250)
+    };
+}
+
+function Cat2() {
+    return {
+    p: new Promise((res,rej) => {
+        pcat2res = res;
+        pcat2rej = rej;
+    }),
+    t : setTimeout(() => pcat2rej("p1"), 250)
+    };
+}
+
+function Timeout(promiseReject){
+    return setTimeout(() => promiseReject("p1"), 250);
+}
+
 app.get('/epoh_data', (req, res) => {
     res.send(paramters);
 })
 
+app.get('/cat_state', (req, res) => {
+    res.send({cat0 : cat0, cat1 : cat1, cat2 : cat2});
+})
+
 app.get('/cat0_toggle', (req, res) => {
     const client = new Client('192.168.100.89', 9000);
-    client.send("/avatar/parameters/cat0", cat0 , () => {
-        cat0 = !cat0;
-        client.close();
+    client.send("/avatar/parameters/cat0", !cat0 , async  () => {
+        let cat = Cat0();
+        cat.p.then( () => { 
+            clearTimeout(cat.t);
+            client.close();
+        }).catch(() => {
+            cat0 = !cat0;
+            client.send("/avatar/parameters/cat0", !cat0 ,  () => {
+                client.close();
+            })
+        })
     })
-    res.end();
+    res.send(cat0);
 })
 
 app.get('/cat1_toggle', (req, res) => {
     const client = new Client('192.168.100.89', 9000);
-    client.send("/avatar/parameters/cat1", cat1 , () => {
-        cat1 = !cat1;
-        client.close();
+    client.send("/avatar/parameters/cat1", !cat1 , async  () => {
+        let cat = Cat1();
+        cat.p.then( () => { 
+            clearTimeout(cat.t);
+            client.close();
+        }).catch(() => {
+            cat1 = !cat1;
+            client.send("/avatar/parameters/cat1", !cat1 ,  () => {
+                client.close();
+            })
+        })
     })
-    res.end();
+    res.send(cat1);
 })
 
 app.get('/cat2_toggle', (req, res) => {
     const client = new Client('192.168.100.89', 9000);
-    client.send("/avatar/parameters/cat2", cat2 , () => {
-        cat2 = !cat2;
-        client.close();
+    client.send("/avatar/parameters/cat2", !cat2 , async  () => {
+        let cat = Cat2();
+        cat.p.then( () => { 
+            clearTimeout(cat.t);
+            client.close();
+        }).catch(() => {
+            cat2 = !cat2;
+            client.send("/avatar/parameters/cat2", !cat2 ,  () => {
+                client.close();
+            })
+        })
     })
-    res.end();
+    res.send(cat2);
 })
 
 
--- public/js/index.js
+++ public/js/index.js
@@ -1,20 +1,40 @@
 
 document.getElementById("cat0").addEventListener("click", () => {
-    axios.get("/cat0_toggle").then(() => {
+    axios.get("/cat0_toggle").then((req, res) => {
         console.log("cat0 toggle");
     })
 })
 
 
 document.getElementById("cat1").addEventListener("click", () => {
-    axios.get("/cat1_toggle").then(() => {
+    axios.get("/cat1_toggle").then((req, res) => {
         console.log("cat1 toggle");
     })
 })
 
 
 document.getElementById("cat2").addEventListener("click", () => {
-    axios.get("/cat2_toggle").then(() => {
+    axios.get("/cat2_toggle").then((req, res) => {
+        console.log("cat2 toggle");
+    })
+})
+
+document.getElementById("cat0off").addEventListener("click", () => {
+    axios.get("/cat0_toggle").then((req, res) => {
+        console.log("cat0 toggle");
+    })
+})
+
+
+document.getElementById("cat1off").addEventListener("click", () => {
+    axios.get("/cat1_toggle").then((req, res) => {
+        console.log("cat1 toggle");
+    })
+})
+
+
+document.getElementById("cat2off").addEventListener("click", () => {
+    axios.get("/cat2_toggle").then((req, res) => {
         console.log("cat2 toggle");
     })
 })
@@ -29,5 +49,33 @@ setInterval(() => {
             para.appendChild(node);
             elm.appendChild(para);
         }
+    });
+    axios.get('cat_state').then((res) => {
+        let cat = res.data;
+        if(cat.cat0){
+            document.getElementById("cat0").style.display = "block";
+            document.getElementById("cat0off").style.display = "none";
+        }
+        else {
+            document.getElementById("cat0").style.display = "none";
+            document.getElementById("cat0off").style.display = "block";
+        }
+        if(cat.cat1){
+            document.getElementById("cat1").style.display = "block";
+            document.getElementById("cat1off").style.display = "none";
+        }
+        else {
+            document.getElementById("cat1").style.display = "none";
+            document.getElementById("cat1off").style.display = "block";
+        }
+        if(cat.cat2){
+            document.getElementById("cat2").style.display = "block";
+            document.getElementById("cat2off").style.display = "none";
+        }
+        else {
+            document.getElementById("cat2").style.display = "none";
+            document.getElementById("cat2off").style.display = "block";
+        }
     })
-}, 100);
+
+}, 250);
